005dff705b91741400d7df3e99311384cac115cd
ryan.veitch <ryan.r.veitch@gmail.com>
2017-02-27 03:02:38 -0600

    Added Realm

--- index.js
+++ index.js
@@ -3,8 +3,7 @@
 var express = require('express');
 var app = express();
 	var bodyParser = require('body-parser');
-	//var multer = require('multer'); // v1.0.5
-	//var upload = multer(); // for parsing multipart/form-data
+var Realm = require('realm');
 var port = Number(process.env.PORT || 3000);
 
 app.use(bodyParser.json()); // for parsing application/json
@@ -12,13 +11,75 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
 app.set('json spaces', 2);
 app.enable('trust proxy');
 
-/* Default Endpoint */
-app.use('*', function (req, res) { //app.get
+var ArticleSchema = {
+  name: 'article',
+	primaryKey: 'id',
+  properties: {
+		id: {type: 'string'},
+    title: {type: 'string', optional: true},
+		published: {type: 'string', optional: true},
+		created: {type: 'string', optional: true},
+		modified: {type: 'string', optional: true},
+		url: {type: 'string', optional: true},
+		body: {type: 'string', optional: true},
+		description: {type: 'string', optional: true},
+  }
+};
+
+let realm = new Realm({
+	schema: [ArticleSchema],
+	path: './realm-data/articles.realm'
+});
+
+/* Publish article endpoint */
+app.use('/publish', function (req, res) {
+
+	realm.write(() => {
+		let newArticle = realm.create('article', {
+			id: req.body.id,
+			title: req.body.title,
+			published: req.body.published,
+			created: req.body.created,
+			modified: req.body.modified,
+			url: req.body.url,
+			body: req.body.body,
+			description: req.body.description,
+		}, true);
+	});
+
 	var responseMessage = {
 		'headers': req.headers,
 		'body': req.body,
 	};
+	
 	console.log(responseMessage);
+	res.json(responseMessage);
+});
+
+/* List Articles in Realm */
+app.get('/articles', function (req, res) {
+	let articles = realm.objects('article');
+	console.log(articles);
+	res.json(articles);
+});
+
+/* Default Endpoint */
+app.use('/', function (req, res) {
+	var responseMessage = {
+		'routes': {
+			'/articles': {
+				path: 'http://localhost/articles',
+				method: 'GET',
+			},
+			'/publish': {
+				path: 'http://localhost/publish',
+				method: 'PUT',
+			}
+		},
+		schema: {
+			article: ArticleSchema
+		}
+	};
 	res.json(responseMessage);
 });
 
