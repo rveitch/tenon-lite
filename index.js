7f652c06c41d4c6dd58c187b9cffa5a0f5fe14a6
ryan.veitch <ryan.r.veitch@gmail.com>
2017-02-27 00:33:35 -0600

    added body parser

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
 
 var express = require('express');
 var app = express();
+	var bodyParser = require('body-parser');
+	//var multer = require('multer'); // v1.0.5
+	//var upload = multer(); // for parsing multipart/form-data
 var port = Number(process.env.PORT || 3000);
 
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.set('json spaces', 2);
 app.enable('trust proxy');
 
